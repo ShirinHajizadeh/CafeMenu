d65341893a46893dda0e1fe8400d3d5a67d6cb75
Shirin Hajizadeh <88985833+ShirinHajizadeh@users.noreply.github.com>
2024-07-31 05:29:07 -0700

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+function DKlanguage() {
+    alert("Jeg glæder mig til at designe en rigtig cafemenu.");
+   }
+   
+   function ENlanguage() {
+    alert("I am looking forward to designing a real cafe menu.");
+   }
+      
+   
